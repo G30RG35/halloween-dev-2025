e0dcadbd822177732adf3b8d0b04ec29ab0a78b7
G30RG35 <javyj2017@hotmail.com>
2025-10-30 22:27:55 -0600

    Ordenamiento

--- Reto_01/Reto_01.js
+++ Reto_01/Reto_01.js
@@ -1,8 +1,7 @@
 console.log("Reto 01: La posesion del codigo");
 function translatePossessed(message) {
-    if (message.trim() === '') {
+    if (message.trim() === '')
         return '';
-    }
     var words = message.split(' ');
     var translatedWords = words.map(function (word) { return word.split('').reverse().join(''); });
     return translatedWords.join(' ');
